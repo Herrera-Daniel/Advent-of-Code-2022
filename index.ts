1ab5fe6e1f30b55e0aa1236f4baeca60f9897689
DanielJHerrera <dan_michigan2012@hotmail.com>
2022-12-08 21:27:02 -0700

    day six part one

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Day 6
+
+const daySix = () => {
+    const fs = require('fs');
+
+    const input: string = fs.readFileSync('input/daysix/daysix.txt', 'ascii');
+
+    console.log(input);
+
+    let marker = 0;
+    input.split('').filter(a => !a.includes('\r') && !a.includes('\n')).forEach((a: string, i: number) => {
+        if (i > 2) {
+            const search = input.substring(i - 3, i + 1);
+            if (!/(.).*\1/.test(search) && marker === 0) {
+                marker = i + 1;
+            }
+        }
+    });
+    console.log(marker);
+};
+
+daySix();
